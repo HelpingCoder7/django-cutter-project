e82024f7f9f3602f197b8c4a78331c04474c402d
sudarshan <sudarshanshrivastava7@gmail.com>
2025-12-17 21:29:17 +0530

    removed user app

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,7 +82,6 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "safetyappbackend.users",
     "safetyappbackend.userauth",
     # Your stuff: custom apps go here
 ]
@@ -102,9 +101,7 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
-AUTH_USER_MODEL = "users.User"
-# https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "users:redirect"
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
